627834d7523776da3bce64e31b27e0c06a610d91
Sirine kriaa <sirine.kriaa@gmail.com>
2017-03-31 20:15:47 +0200

    modify ws

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.post('/register',function(req,res){
 
 //get preference by event
 app.get('/event/preference', function(req,res){
-  connection.query("SELECT * FROM preference WHERE idevent = 2"
+  connection.query("select users.couleur, preference.datepref from users,preference where preference.idevent=2 and users.login=preference.iduser"
 , function(error,rows,field){
     if(!!error){
       console.log('Error in the query');
